f5e91e48c08d6789a0f81950c0de391db1fbd426
unknown <j.rivera@powerdigitalmarketing.com>
2024-05-22 19:48:23 -0700

    fixed step 2

--- js/math.js
+++ js/math.js
@@ -1,9 +1,32 @@
 //STEP 1
 
+// let num = +window.prompt("Enter a Number");
+// num = Math.abs(num);
+// window.alert(num);
+
+
 //STEP 2
+
+// let num2 = +window.prompt("Enter a Floating Point Number");
+// num2 = Math.ceil(num2);
+// window.alert(num2);
 
 //STEP 3
 
+// let num3 = +window.prompt("Enter a Floating Point Number");
+// num3 = Math.floor(num3);
+// window.alert(num3);
+
 //STEP 4
 
-//STEP 5
+// let num4 = window.prompt("Enter a 5 Numbers");
+// let max = ;
+// window.alert(num4);
+
+//STEP 5 Math.sqrt(9)
+
+let num5 = window.prompt("Enter a Number");
+num5 = Math.sqrt(num5);
+window.alert(num5);
+
+
--- js/strings.js
+++ js/strings.js
@@ -1,59 +1,59 @@
 //STEP 1
 
-// let userName = window.prompt("What is your Name?");
-// window.alert('Your name length is ' + userName.length);
+let userName = window.prompt("What is your Name?");
+window.alert('Your name length is ' + userName.length);
 
 //STEP 2
 
-// let userName2 = window.prompt("What is your Name?");
-// let stringIndex = +window.prompt("Enter Number");
-// window.alert('Your name length is ' + userName2[stringIndex] );
+let userName2 = window.prompt("What is your Name?");
+let stringIndex = +window.prompt("Enter Number");
+window.alert('Your name length is ' + userName2[stringIndex] );
 
 //STEP 3
 
-// let userFistName = window.prompt("What is your first name?");
-// let userLastName = window.prompt("What is your last name?");
-// let fullname = userFistName.concat(userLastName);
-// window.alert('Your name is ' + fullname );
+let userFistName = window.prompt("What is your first name?");
+let userLastName = window.prompt("What is your last name?");
+let fullname = userFistName.concat(userLastName);
+window.alert('Your name is ' + fullname );
 
 //STEP 4
 
-// const str = "The quick brown fox jumps over the lazy dog";
-// let foxIndex = str.indexOf('fox');
-//  window.alert(foxIndex);
+const str = "The quick brown fox jumps over the lazy dog";
+let foxIndex = str.indexOf('fox');
+ window.alert(foxIndex);
 
 
 //STEP 5
 
-// const str2 = "The quick brown fox jumps over the lazy dog";
-// let foxLastIndex = str2.lastIndexOf('fox');
-// window.alert(foxLastIndex);
+const str2 = "The quick brown fox jumps over the lazy dog";
+let foxLastIndex = str2.lastIndexOf('fox');
+window.alert(foxLastIndex);
 
 //STEP 6
 
-// const str3 = "The quick brown fox jumps over the lazy dog";
-// let word = window.prompt("Enter Word");
-// window.alert(str3.replace('the lazy dog', word));
+const str3 = "The quick brown fox jumps over the lazy dog";
+let word = window.prompt("Enter Word");
+window.alert(str3.replace('the lazy dog', word));
 
 //STEP 7
 
-// const str4 = "The quick brown fox jumps over the lazy dog";
-// let word2 = window.prompt("Enter Word");
-// let search = str4.search(word2)
-// window.alert(search);
+const str4 = "The quick brown fox jumps over the lazy dog";
+let word2 = window.prompt("Enter Word");
+let search = str4.search(word2)
+window.alert(search);
 
 //STEP 8
 
-// const old_str = "The quick brown fox jumps over the lazy dog";
-// let strIndex = old_str.indexOf('the lazy dog');
-// let new_str = old_str.substring(strIndex);
-// window.alert(new_str);
+const old_str = "The quick brown fox jumps over the lazy dog";
+let strIndex = old_str.indexOf('the lazy dog');
+let new_str = old_str.substring(strIndex);
+window.alert(new_str);
 
 //STEP 9
 
-// const old_str2 = " THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          "
-// let new_str2 = old_str2.toLowerCase().trim();
-// window.alert(new_str2);
+const old_str2 = " THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          "
+let new_str2 = old_str2.toLowerCase().trim();
+window.alert(new_str2);
 
 //STEP 10
 
